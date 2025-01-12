f790d3974b340891585d52087e8dd97aa006cb78
JayGhz <jaykansas7@gmail.com>
2025-01-11 23:15:41 -0500

    add Neovim configuration with basic settings for line numbers and indentation

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,9 @@
+local vim
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
